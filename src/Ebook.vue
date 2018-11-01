1054127ce3905aef85f36010e9f6da2f3ac61c3c
MrZWH <1875290692@qq.com>
2018-11-01 23:03:05 +0800

    字号设置功能实现

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,9 +1,6 @@
 <template>
 	<div class="ebook">
-		<transition name="slide-down">
-
-			
-		</transition>
+		<title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
 		<div class="read-wrapper">
 			<div id="read"></div>
 			<div class="mask">
@@ -12,146 +9,109 @@
 				<div class="right" @click="nextPage"></div>
 			</div>
 		</div>
-		<transition name="slide-up">
-			<div class="menu-wrapper" v-show="ifTitleAndMenuShow">
-				<div class="icon-wrapper">
-					<span class="icon-menu icon"></span>
-				</div>
-				<div class="icon-wrapper">
-					<span class="icon-progress icon"></span>
-				</div>
-				<div class="icon-wrapper">
-					<span class="icon-bright icon"></span>
-				</div>
-				<div class="icon-wrapper">
-					<span class="icon-a icon"></span>
-				</div>
-			</div>
-		</transition>
+		<menu-bar @setFontSize="setFontSize" :defaultFontSize="defaultFontSize" :fontSizeList="fontSizeList" :ifTitleAndMenuShow="ifTitleAndMenuShow" ref="menuBar"></menu-bar>
 	</div>
 </template>
 
 <script>
-import Epub from 'epubjs'
-const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
-global.ePub = Epub
+import TitleBar from '@/components/TitleBar'
+import MenuBar from "@/components/MenuBar";
+import Epub from "epubjs";
+const DOWNLOAD_URL = "/static/2018_Book_AgileProcessesInSoftwareEngine.epub";
+global.ePub = Epub;
 
 export default {
-	data() {
-		return {
-			ifTitleAndMenuShow: false
-		}
+	components: {
+		TitleBar,
+		MenuBar
 	},
-	methods: {
-		toggleTitleAndMenu() {
+  data() {
+    return {
+      ifTitleAndMenuShow: false,
+      fontSizeList: [
+        {fonstSize: 12},
+        {fonstSize: 14},
+        {fonstSize: 16},
+        {fonstSize: 18},
+        {fonstSize: 20},
+        {fonstSize: 22},
+        {fonstSize: 24},
+      ],
+      defaultFontSize: 16,
+    };
+  },
+  methods: {
+    setFontSize(fontSize) {
+      this.defaultFontSize = fontSize;
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
+      }
+    },
+    toggleTitleAndMenu() {
 			this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow;
-		},
-		prevPage() {
-			// Rendition.prev
-			if (this.rendition) {
-				this.rendition.prev()
+			if(!this.ifTitleAndMenuShow) {
+				this.$refs.menuBar.hideSetting()
 			}
-		},
-		nextPage() {
-			if (this.rendition) {
-				this.rendition.next()
-			}
+    },
+    prevPage() {
+      // Rendition.prev
+      if (this.rendition) {
+        this.rendition.prev();
+      }
+    },
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next();
+      }
+    },
+    // 电子书的解析和渲染
+    showEpub() {
+      // 生成 Book 对象
+      this.book = new Epub(DOWNLOAD_URL);
+      // 生成 Rendition 通过 Book.renderTo
+      this.rendition = this.book.renderTo("read", {
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+      // 通过 Rendition.display 渲染电子书
+      this.rendition.display();
 
-		},
-		// 电子书的解析和渲染
-		showEpub () {
-			// 生成 Book 对象
-			this.book = new Epub(DOWNLOAD_URL)
-			// 生成 Rendition 通过 Book.renderTo
-			this.rendition = this.book.renderTo('read', {
-				width: window.innerWidth,
-				height: window.innerHeight
-			})
-			// 通过 Rendition.display 渲染电子书
-			this.rendition.display()
-		}
-	},
-	mounted () {
-		this.showEpub()
-	}
-}
+      // 获取 Theme 对象
+      this.themes = this.rendition.themes
+      // 设置默认字体
+      this.setFontSize(this.defaultFontSize)
+    }
+  },
+  mounted() {
+    this.showEpub();
+  }
+};
 </script>
 
 <style lang='scss' scoped>
-@import 'assets/styles/global';
+@import "assets/styles/global";
 
 .ebook {
-	position: relative;
-	.title-wrapper {
-		position: absolute;
-		top: 0;
-		left: 0;
-		z-index: 101;
-		display: flex;
-		width: 100%;
-		height: px2rem(48);
-		background: white;
-		box-shadow: 0 px2rem(8) px2rem(8) rgba(0,0,0,.15);
-		.left {
-			flex: 0 0 px2rem(60);
-			@include center;
-		}
-		.right {
-			display: flex;
-			justify-content: flex-end;
-			flex: 1;
-			.icon-wrapper {
-				@include center;
-				flex: 0 0 px2rem(40);
-				.icon-cart {
-					font-size: px2rem(22);
-				}
-			}
-		}
-	}
-	.read-wrapper {
-		.mask {
-			position: absolute;
-			top: 0;
-			left: 0;
-			z-index: 100;
-			width: 100%;
-			height: 100%;
-			display: flex;
-			.left {
-				flex: 0 0 px2rem(100);
-			}
-			.center {
-				flex: 1;				
-			}
-			.right {
-				flex: 0 0 px2rem(100);
-			}
-		}
-	}
-	.menu-wrapper {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		z-index: 101;
-		display: flex;
-		width: 100%;
-		height: px2rem(48);
-		background: white;
-		box-shadow: 0 px2rem(-8) px2rem(8) rgba(0,0,0,.15);
-
-		.icon-wrapper {
-			flex: 1;
-			@include center;
-
-			.icon-progress {
-				font-size: px2rem(28)
-			}
-
-			.icon-bright {
-				font-size: px2rem(24)
-			}
-		}
-	}
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      .left {
+        flex: 0 0 px2rem(100);
+      }
+      .center {
+        flex: 1;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+      }
+    }
+  }
 }
 </style>
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,3 +1,175 @@
 <template>
-  
+  <div class="menu-bar">
+    <transition name="slide-up">
+      <div class="menu-wrapper" :class="{'hide-box-shadow': ifSettingShow || !ifTitleAndMenuShow}" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-a icon" @click="showSetting">A</span>
+        </div>
+      </div>
+    </transition>
+    <transition name="slide-up">
+      <div class="setting-wrapper" v-show="ifSettingShow">
+        <div class="setting-font-size">
+          <div class="preview" :style="{fontSize: fontSizeList[0].fontSize + 'px'}">A</div>
+          <div class="select">
+            <div class="select-wrapper" @click="setFontSize(item.fontSize)" v-for="(item, index) in fontSizeList" :key="index">
+              <div class="line"></div>
+              <div class="point-wrapper">
+                <div class="point" v-show="defaultFontSize === item.fontSize">
+                  <div class="small-point"></div>
+                </div>
+              </div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="preview" :style="{fontSize: fontSizeList[fontSizeList.length - 1].fontSize + 'px'}">A</div>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    },
+    fontSizeList: Array,
+    defaultFontSize: Number
+  },
+  data() {
+    return {
+      ifSettingShow: false
+    }
+  },
+  method: {
+    setFontSize(fontSize) {
+      this.$emit('setFontSize', fontSize)
+    },
+    showSetting() {
+      this.ifSettingShow = true;
+    },
+    hideSetting() {
+      this.ifSettingShow = false;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/global";
+.menu-bar {
+  .menu-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: px2rem(48);
+    background: white;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+
+    &.hide-box-shadow {
+      box-shadow: none;
+    }
+
+    .icon-wrapper {
+      flex: 1;
+      @include center;
+
+      .icon-progress {
+        font-size: px2rem(28);
+      }
+
+      .icon-bright {
+        font-size: px2rem(24);
+      }
+    }
+  }
+  .setting-wrapper {
+    z-index: 101;
+    position: absolute;
+    bottom: px2rem(48);
+    width: 100%;
+    height: px2rem(60);
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    background: white;
+    .setting-font-size {
+      display: flex;
+      height: 100%;
+      .preview {
+        flex: 0 0 px2rem(40);
+        @include center;
+      }
+      .select {
+        display: flex;
+        flex: 1;
+        .select-wrapper {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          &:first-child {
+            .line {
+              &:first-child {
+                border-top: none;
+              }
+            }
+          }
+
+          &:last-child {
+            .line {
+              &:last-child {
+                border-top: none;
+              }
+            }
+          }
+  
+          .line {
+            flex: 1;
+            height: 0;
+            border-top: px2rem(1) solid #ccc;
+          }
+  
+          .point-wrapper {
+            flex: 0 0 0;
+            width: 0;
+            height: px2rem(7);
+            border-left: px2rem(1) solid #ccc;
+            position: relative;
+            .point {
+              position: absolute;
+              top: px2rem(-8);
+              left: px2rem(-10);
+              width: px2rem(20);
+              height: px2rem(20);
+              border-radius: 50%;
+              background: white;
+              border: px2rem(1) solid #ccc;
+              box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, .15);
+              @include center;
+              .small-point {
+                width: px2rem(5);
+                height: px2rem(5);
+                border-radius: 50%;
+                background: #000;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
